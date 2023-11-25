dc4128a0e4966757bb5cdf77f1cef560d80f892c
Kevin Pfefferle <kevin@pfefferle.me>
2023-11-24 23:56:53 -0500

    Clean slate

--- 01-WeSplit/WeSplit/ContentView.swift
+++ 01-WeSplit/WeSplit/ContentView.swift
@@ -8,19 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"]
-    @State private var selectedStudent = "Harry"
-    
     var body: some View {
-        NavigationStack {
-            Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
-                    }
-                }
-            }
-        }
+        Text("Hello, world!")
     }
 }
 
