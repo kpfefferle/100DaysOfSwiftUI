2cba49d3e09a2da7eeccfb8b1909744b4e2cb281
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-02 13:22:28 -0500

    Init GuessTheFlag

---
+++ 02-GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Kevin Pfefferle on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
