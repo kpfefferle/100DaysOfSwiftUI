8c3e2422df17c415fb24b05175aa3f7844569d30
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-02 14:34:40 -0500

    Experimenting with subtle gradients

--- 02-GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 02-GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,11 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            AngularGradient(colors: [.red, .orange, .yellow, .green, .blue, .purple, .red], center: .center)
             Text("Your content")
-                .foregroundStyle(.secondary)
-                .padding(50)
-                .background(.ultraThinMaterial)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .foregroundStyle(.white)
+                .background(.red.gradient)
         }
         .ignoresSafeArea()
     }
