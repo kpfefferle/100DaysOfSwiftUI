86687741fcdcbb958faf50a06d4855c81d1337fb
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-02 15:32:13 -0500

    Refine design

--- 02-GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 02-GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -27,13 +27,15 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
+                        .font(.subheadline.weight(.heavy))
                         .foregroundStyle(.white)
                     Text(countries[correctAnswer])
+                        .font(.largeTitle.weight(.semibold))
                         .foregroundStyle(.white)
                 }
                 
@@ -42,6 +44,8 @@ struct ContentView: View {
                         flagTapped(number)
                     } label: {
                         Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
                     }
                 }
             }
