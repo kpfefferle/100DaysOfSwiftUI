609f8ec9caeefc2de12c0e0f23286c0c9996ee6f
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-03 14:29:37 -0500

    Custom modifier with new view structure

--- 03-ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ 03-ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,27 +7,32 @@
 
 import SwiftUI
 
-struct Title: ViewModifier {
+struct Watermark: ViewModifier {
+    var text: String
+
     func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundStyle(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(.rect(cornerRadius: 10))
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
     }
 }
 
 extension View {
-    func titleStyle() -> some View {
-        modifier(Title())
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
-            .titleStyle()
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
     }
 }
 
