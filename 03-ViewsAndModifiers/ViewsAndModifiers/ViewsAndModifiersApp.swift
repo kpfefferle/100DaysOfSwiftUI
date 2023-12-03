e01b1c5113e637c64d89c1d1cb5829fe8c1ac19c
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-03 13:47:34 -0500

    Init ViewsAndModifiers

---
+++ 03-ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Kevin Pfefferle on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
