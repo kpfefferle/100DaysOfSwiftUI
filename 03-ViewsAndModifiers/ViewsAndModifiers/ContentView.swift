c573a4f9129ce6d655d9ab53d9f5dfc2b7316395
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-03 13:56:52 -0500

    Order of modifiers

--- 03-ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ 03-ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -9,14 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
-        .background(.red)
+        Text("Hello, world!")
+            .padding()
+            .background(.red)
+            .padding()
+            .background(.blue)
+            .padding()
+            .background(.green)
+            .padding()
+            .background(.yellow)
     }
 }
 
