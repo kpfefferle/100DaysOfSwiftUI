31e10187eaa3dc4eb9edfd378481f427d5dd37b4
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-05 00:23:00 -0500

    Basic layout

--- C02-RPSTrainer/RPSTrainer/ContentView.swift
+++ C02-RPSTrainer/RPSTrainer/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    let moves = ["✊🏻", "✋🏻", "✌🏻"].shuffled()
+    @State var opponentMove = Int.random(in: 0...2)
+    @State var userShouldWin = Bool.random()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.gray
+                .ignoresSafeArea()
+            
+            VStack(spacing: 8) {
+                Spacer()
+
+                Text("Your opponent's move is")
+                Text(moves[opponentMove])
+                    .font(.system(size: 100))
+
+                Spacer()
+                
+                Text("What is your move to...")
+                Text("\(userShouldWin ? "Win" : "Lose")?")
+                    .font(.title.bold())
+                
+                ForEach(moves, id: \.self) { move in
+                    Button {
+                        // check answer
+                    } label: {
+                        Text(move)
+                            .font(.system(size: 100))
+                    }
+                }
+
+                Spacer()
+            }
+            .foregroundStyle(.white)
+            .padding()
         }
-        .padding()
     }
 }
 
