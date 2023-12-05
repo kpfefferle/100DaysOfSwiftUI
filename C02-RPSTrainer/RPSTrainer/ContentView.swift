b5a183741edf90cf98048f74d75147508ac60f15
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-05 00:06:56 -0500

    Init RPSTrainer

---
+++ C02-RPSTrainer/RPSTrainer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RPSTrainer
+//
+//  Created by Kevin Pfefferle on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ C02-RPSTrainer/RPSTrainer/RPSTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSTrainerApp.swift
+//  RPSTrainer
+//
+//  Created by Kevin Pfefferle on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RPSTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
