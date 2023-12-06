94b0bdeda0958ce1e3abdb22001db0fda75fc1d7
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-06 16:41:58 -0500

    Drag with implicit animation

--- 06-Animations/Animations/ContentView.swift
+++ 06-Animations/Animations/ContentView.swift
@@ -20,6 +20,7 @@ struct ContentView: View {
                     .onChanged { dragAmount = $0.translation }
                     .onEnded { _ in dragAmount = .zero }
             )
+            .animation(.bouncy, value: dragAmount)
     }
 }
 
