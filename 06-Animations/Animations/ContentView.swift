52af23558dace4fa2341413e15b11e96d4dfb2c1
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-06 16:13:49 -0500

    Stepper with animation of changes

--- 06-Animations/Animations/ContentView.swift
+++ 06-Animations/Animations/ContentView.swift
@@ -11,26 +11,19 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     
     var body: some View {
-        Button("Tap Me") {
-            // animationAmount += 1
-        }
-        .padding(50)
-        .background(.red)
-        .foregroundStyle(.white)
-        .clipShape(.circle)
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .animation(
-                    .easeInOut(duration: 1)
-                        .repeatForever(autoreverses: false),
-                    value: animationAmount
-                )
-        )
-        .onAppear {
-            animationAmount = 2
+        VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(), in: 1...10)
+            
+            Spacer()
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(40)
+            .background(.red)
+            .foregroundStyle(.white)
+            .clipShape(.circle)
+            .scaleEffect(animationAmount)
         }
     }
 }
