510703cd2ff2e5ee0eb5c67e7b9472e81cefe2fc
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-06 14:30:35 -0500

    Init WordScramble

---
+++ 05-WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Kevin Pfefferle on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
