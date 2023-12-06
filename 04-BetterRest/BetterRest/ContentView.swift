36f9145dd4b5964f41b386db4278d145b24b2cfd
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-06 10:09:23 -0500

    Basic layout

--- 04-BetterRest/BetterRest/ContentView.swift
+++ 04-BetterRest/BetterRest/ContentView.swift
@@ -8,16 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
-    
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+
     var body: some View {
-        VStack {
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            
-            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-                .labelsHidden()
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
         }
+    }
+    
+    func calculateBedtime() {
     }
 }
 
