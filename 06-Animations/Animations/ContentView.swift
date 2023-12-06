4d0b7729de8d096b583a272c78aee0d40d0709cb
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-06 16:40:27 -0500

    Create draggable card

--- 06-Animations/Animations/ContentView.swift
+++ 06-Animations/Animations/ContentView.swift
@@ -8,18 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        Button("Tap Me") {
-            enabled.toggle()
-        }
-        .padding(50)
-        .background(enabled ? .blue : .red)
-        .animation(nil, value: enabled)
-        .foregroundStyle(.white)
-        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
-        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
+        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded { _ in dragAmount = .zero }
+            )
     }
 }
 
