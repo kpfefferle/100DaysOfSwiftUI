04e4309298975cfb2ca6c39034159c8e858b426a
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-06 16:49:15 -0500

    Combine animations with drag gestures for letter snake

--- 06-Animations/Animations/ContentView.swift
+++ 06-Animations/Animations/ContentView.swift
@@ -8,22 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello SwiftUI")
+    @State private var enabled = false
     @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(.rect(cornerRadius: 10))
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged { dragAmount = $0.translation }
-                    .onEnded { _ in
-                        withAnimation(.bouncy) {
-                            dragAmount = .zero
-                        }
-                    }
-            )
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { letterIndex in
+                Text(String(letters[letterIndex]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .red)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(letterIndex) / 20), value: dragAmount)
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
     }
 }
 
