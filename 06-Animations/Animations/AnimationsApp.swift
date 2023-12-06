eef4e66b75e8e4c635eefd9ce595a17c571a9d70
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-06 15:54:48 -0500

    Init Animations

---
+++ 06-Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Kevin Pfefferle on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
