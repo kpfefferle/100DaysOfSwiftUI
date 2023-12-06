25754237e360cbd285591ee8d21cf6107f0609ef
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-06 10:42:16 -0500

    Calculate sleep amount

--- 04-BetterRest/BetterRest/ContentView.swift
+++ 04-BetterRest/BetterRest/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Kevin Pfefferle on 12/6/23.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -32,6 +33,20 @@ struct ContentView: View {
     }
     
     func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            let prediction = try model.prediction(wake: Int64(hour + minute), estimatedSleep: sleepAmount, coffee: Int64(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+        } catch {
+            // something went wrong!
+        }
     }
 }
 
