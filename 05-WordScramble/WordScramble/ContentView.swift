5136d5012e22ea290fef8cc6f56c130a2334e244
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-06 15:25:36 -0500

    Prompt for random word from text file

--- 05-WordScramble/WordScramble/ContentView.swift
+++ 05-WordScramble/WordScramble/ContentView.swift
@@ -31,7 +31,20 @@ struct ContentView: View {
             }
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
         }
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from the bundle.")
     }
     
     func addNewWord() {
