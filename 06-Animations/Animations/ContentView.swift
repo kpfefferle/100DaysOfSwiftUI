2bb34234752ce292908bb1eb0f8434efd918e3be
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-06 16:29:58 -0500

    Use animation to toggle button color

--- 06-Animations/Animations/ContentView.swift
+++ 06-Animations/Animations/ContentView.swift
@@ -8,19 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
         Button("Tap Me") {
-            withAnimation(.spring(duration: 1, bounce: 0.5)) {
-                animationAmount += 360
-            }
+            enabled.toggle()
         }
         .padding(50)
-        .background(.red)
+        .background(enabled ? .blue : .red)
         .foregroundStyle(.white)
         .clipShape(.circle)
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
+        .animation(.default, value: enabled)
     }
 }
 
