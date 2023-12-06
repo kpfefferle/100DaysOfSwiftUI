cf5274fe56232f7a2a24c3f2f2abd134a4a199fe
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-06 00:00:36 -0500

    Init BetterRest

---
+++ 04-BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kevin Pfefferle on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
