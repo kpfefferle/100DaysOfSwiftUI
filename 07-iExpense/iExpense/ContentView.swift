b67831fe6c2c4c9f9b5e5a50682fdb4d4abc9747
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-09 14:17:22 -0500

    Present a sheet

--- 07-iExpense/iExpense/ContentView.swift
+++ 07-iExpense/iExpense/ContentView.swift
@@ -7,23 +7,22 @@
 
 import SwiftUI
 
-@Observable
-class User {
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
-}
-
 struct ContentView: View {
-    @State private var user = User()
+    @State private var showingSheet = false
     
     var body: some View {
-        VStack {
-            Text("Your name is \(user.firstName) \(user.lastName)")
-            
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
+        Button("Show Sheet") {
+            showingSheet.toggle()
         }
-        .padding()
+        .sheet(isPresented: $showingSheet) {
+            SecondView()
+        }
+    }
+}
+
+struct SecondView: View {
+    var body: some View {
+        Text("Second View")
     }
 }
 
