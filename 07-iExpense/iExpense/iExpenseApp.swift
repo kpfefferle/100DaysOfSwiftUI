c2150beb8d2802c2d7d1977bc5f7145f6ef3fa4b
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-09 14:04:20 -0500

    Init iExpense

---
+++ 07-iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Kevin Pfefferle on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
