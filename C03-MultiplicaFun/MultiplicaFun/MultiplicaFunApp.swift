5fac236500c78799d66099d58e5d7d9c384d85dd
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-09 14:00:54 -0500

    Init MultiplicaFun

---
+++ C03-MultiplicaFun/MultiplicaFun/MultiplicaFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicaFunApp.swift
+//  MultiplicaFun
+//
+//  Created by Kevin Pfefferle on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicaFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
