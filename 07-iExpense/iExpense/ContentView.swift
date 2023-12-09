945d73c87cdf429027757af7edf6bc5c7fc8e1b8
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-09 14:41:57 -0500

    onDelete with ForEach

--- 07-iExpense/iExpense/ContentView.swift
+++ 07-iExpense/iExpense/ContentView.swift
@@ -8,25 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView()
+        VStack {
+            List {
+                ForEach(numbers, id: \.self) {
+                    Text("Row \($0)")
+                }
+                .onDelete(perform: removeRows)
+            }
+            
+            Button("Add Number") {
+                numbers.append(currentNumber)
+                currentNumber += 1
+            }
         }
     }
-}
-
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
     
-    var body: some View {
-        Button("Dismiss") {
-            dismiss()
-        }
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
