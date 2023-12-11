736d17ea37a7b333bf5faa10b78d036d59d76eda
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-10 23:17:57 -0500

    Init Moonshot

---
+++ 08-Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Kevin Pfefferle on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
