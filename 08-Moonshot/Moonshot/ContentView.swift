89296846577be309868f88440a7df127dd02146b
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-10 23:35:18 -0500

    LazyHStack

--- 08-Moonshot/Moonshot/ContentView.swift
+++ 08-Moonshot/Moonshot/ContentView.swift
@@ -22,14 +22,13 @@ struct CustomText: View {
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            LazyVStack(spacing: 10) {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
                 ForEach(0..<100) {
                     CustomText("Item \($0)")
                         .font(.title)
                 }
             }
-            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
         }
     }
 }
