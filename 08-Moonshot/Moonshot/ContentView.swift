5f2d5c14a866f7ebf3552d9cdece1608b70de591
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-10 23:32:52 -0500

    ScrollView

--- 08-Moonshot/Moonshot/ContentView.swift
+++ 08-Moonshot/Moonshot/ContentView.swift
@@ -7,14 +7,30 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText: \(text)")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Image(.apollo11)
-            .resizable()
-            .scaledToFit()
-            .containerRelativeFrame(.horizontal) { size, axis in
-                size * 0.8
+        ScrollView {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item \($0)")
+                        .font(.title)
+                }
             }
+            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
