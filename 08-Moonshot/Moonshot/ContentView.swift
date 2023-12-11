d6babafdc290f5b68064c8f1445186341ca45826
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-10 23:37:37 -0500

    Basic NavigationLink

--- 08-Moonshot/Moonshot/ContentView.swift
+++ 08-Moonshot/Moonshot/ContentView.swift
@@ -7,28 +7,13 @@
 
 import SwiftUI
 
-struct CustomText: View {
-    let text: String
-    
-    var body: some View {
-        Text(text)
-    }
-    
-    init(_ text: String) {
-        print("Creating a new CustomText: \(text)")
-        self.text = text
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText("Item \($0)")
-                        .font(.title)
-                }
+        NavigationStack {
+            NavigationLink("Tap Me") {
+                Text("Detail View")
             }
+            .navigationTitle("SwiftUI")
         }
     }
 }
