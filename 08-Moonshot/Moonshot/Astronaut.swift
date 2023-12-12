952eeaff3a63d24a19b0fb78ef387adaef28ca6e
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-12 00:12:47 -0500

    Create Astronaut struct

---
+++ 08-Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Kevin Pfefferle on 12/12/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
