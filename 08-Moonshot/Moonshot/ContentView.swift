24086abd171d591ea9f17301329ef07df3744b09
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-12 00:45:56 -0500

    Refine design with custom colors

--- 08-Moonshot/Moonshot/ContentView.swift
+++ 08-Moonshot/Moonshot/ContentView.swift
@@ -28,20 +28,33 @@ struct ContentView: View {
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                                    .padding()
                                 
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
+                                        .foregroundStyle(.white)
                                     Text(mission.formattedLaunchDate)
                                         .font(.caption)
+                                        .foregroundStyle(.white.opacity(0.5))
                                 }
+                                .padding(.vertical)
                                 .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
                             }
+                            .clipShape(.rect(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
                         }
                     }
                 }
+                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
         }
     }
 }
