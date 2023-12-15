a3a33991a63b3e9b8f8e8ed2af558ce688346bdc
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-15 15:46:07 -0500

    Editable navigationTitle with binding

--- 09-Navigation/Navigation/ContentView.swift
+++ 09-Navigation/Navigation/ContentView.swift
@@ -8,20 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var title = "SwiftUI"
+
     var body: some View {
         NavigationStack {
             Text("Hello, world!")
-                .toolbar {
-                    ToolbarItemGroup(placement: .topBarLeading) {
-                        Button("Tap Me") {
-                            // button action here
-                        }
-
-                        Button("Or Tap Me") {
-                            // button action here
-                        }
-                    }
-                }
+                .navigationTitle($title)
+                .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
