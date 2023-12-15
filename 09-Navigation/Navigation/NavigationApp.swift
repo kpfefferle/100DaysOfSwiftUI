4b729db469e539155a5a96ef1969b08e066d085e
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-15 13:54:18 -0500

    Init Navigation

---
+++ 09-Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Kevin Pfefferle on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
