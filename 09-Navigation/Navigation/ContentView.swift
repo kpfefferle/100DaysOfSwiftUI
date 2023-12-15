97b7a577f5c86f7d2d0ca5e84d9b4eb30d681805
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-15 15:27:25 -0500

    Inline navigation title style

--- 09-Navigation/Navigation/ContentView.swift
+++ 09-Navigation/Navigation/ContentView.swift
@@ -7,30 +7,14 @@
 
 import SwiftUI
 
-struct DetailView: View {
-    var number: Int
-    @Binding var path: NavigationPath
-    
-    var body: some View {
-        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
-            .navigationTitle("Number: \(number)")
-            .toolbar {
-                Button("Home") {
-                    path = NavigationPath()
-                }
-            }
-    }
-}
-
 struct ContentView: View {
-    @State private var pathStore = PathStore()
-    
     var body: some View {
-        NavigationStack(path: $pathStore.path) {
-            DetailView(number: 0, path: $pathStore.path)
-                .navigationDestination(for: Int.self) { i in
-                    DetailView(number: i, path: $pathStore.path)
-                }
+        NavigationStack {
+            List(0..<100) { i in
+                Text("Row \(i)")
+            }
+            .navigationTitle("Title goes here")
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
