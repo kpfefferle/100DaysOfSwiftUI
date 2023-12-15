1223ef44d1fccf668e142b416442503de1c7477f
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-15 15:32:25 -0500

    Toolbar button with explicit placement

--- 09-Navigation/Navigation/ContentView.swift
+++ 09-Navigation/Navigation/ContentView.swift
@@ -10,14 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            List(0..<100) { i in
-                Text("Row \(i)")
-            }
-            .navigationTitle("Title goes here")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbarBackground(.blue, for: .navigationBar)
-            .toolbarColorScheme(.dark, for: .navigationBar)
-            .toolbar(.hidden, for: .navigationBar)
+            Text("Hello, world!")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button("Tap Me") {
+                            // button action here
+                        }
+                    }
+                }
         }
     }
 }
