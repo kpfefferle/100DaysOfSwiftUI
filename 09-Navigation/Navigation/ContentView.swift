c011cecca50c47ffc11b82c44a88a4fee3ebe7e1
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-15 14:58:06 -0500

    Infinite random number detail views

--- 09-Navigation/Navigation/ContentView.swift
+++ 09-Navigation/Navigation/ContentView.swift
@@ -7,28 +7,24 @@
 
 import SwiftUI
 
+struct DetailView: View {
+    var number: Int
+    
+    var body: some View {
+        NavigationLink("Go to Random Number", value: Int.random(in: 1...1000))
+            .navigationTitle("Number: \(number)")
+    }
+}
+
 struct ContentView: View {
-    @State private var path = NavigationPath()
+    @State private var path = [Int]()
     
     var body: some View {
         NavigationStack(path: $path) {
-            VStack {
-                Button("Show 32") {
-                    path.append(32)
+            DetailView(number: 0)
+                .navigationDestination(for: Int.self) { i in
+                    DetailView(number: i)
                 }
-                
-                Button("Show 64") {
-                    path.append(64)
-                }
-                
-                Button("Show 32 then 64") {
-                    path.append(32)
-                    path.append(64)
-                }
-            }
-            .navigationDestination(for: Int.self) { selection in
-                Text("You selected \(selection)")
-            }
         }
     }
 }
