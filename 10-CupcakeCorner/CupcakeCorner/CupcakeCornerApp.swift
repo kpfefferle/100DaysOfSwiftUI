6e6194552dfd8768cccef430a32d68a3dc2966b0
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-16 13:32:17 -0500

    Init CupcakeCorner

---
+++ 10-CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Kevin Pfefferle on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
