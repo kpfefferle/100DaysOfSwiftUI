f2b69f3eb183d55f44f98a2d819662210d068f2b
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-15 23:25:26 -0500

    Init Habit

---
+++ C04-Habit/Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Kevin Pfefferle on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
