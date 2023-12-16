df4d6ff31c10d1a5932114314675b477a3958a82
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-16 14:59:20 -0500

    Custom haptic event

--- 10-CupcakeCorner/CupcakeCorner/ContentView.swift
+++ 10-CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -5,16 +5,46 @@
 //  Created by Kevin Pfefferle on 12/16/23.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var counter = 0
-    
+    @State private var engine: CHHapticEngine?
+
     var body: some View {
-        Button("Tap count: \(counter)") {
-            counter += 1
+        Button("Tap me", action: complexSuccess)
+            .onAppear(perform: prepareHaptics)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
         }
-        .sensoryFeedback(.increase, trigger: counter)
+    }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        // create one intense, sharp tap
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription)")
+        }
     }
 }
 
