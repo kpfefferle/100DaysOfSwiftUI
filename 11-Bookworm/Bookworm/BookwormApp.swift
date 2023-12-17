f30225d0046fd5832986b94eb6a51b0209879f64
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-17 13:54:42 -0500

    Init Bookworm

---
+++ 11-Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Kevin Pfefferle on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
