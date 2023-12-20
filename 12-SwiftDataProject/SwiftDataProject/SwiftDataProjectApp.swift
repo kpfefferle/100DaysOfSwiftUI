2937062fac9ec8f5c2bf638577293b6868109b1b
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-20 10:41:25 -0500

    Init SwiftDataProject

---
+++ 12-SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Kevin Pfefferle on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
