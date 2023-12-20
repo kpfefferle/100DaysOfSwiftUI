78f3c15cf9fdf33bc5f840e2e4e2d037c35f7e2e
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-20 11:02:49 -0500

    Add User with edit view reused for new Users

--- 12-SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
+++ 12-SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -5,6 +5,7 @@
 //  Created by Kevin Pfefferle on 12/20/23.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct SwiftDataProjectApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: User.self)
     }
 }
