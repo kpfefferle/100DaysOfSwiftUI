6ccb918b4e873939f5c70d251cf0e11dea428109
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-20 11:25:51 -0500

    Dynamic sorting

--- 12-SwiftDataProject/SwiftDataProject/ContentView.swift
+++ 12-SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -12,14 +12,34 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     
     @State private var showUpcomingOnly = false
+    @State private var sortOrder = [
+        SortDescriptor(\User.name),
+        SortDescriptor(\User.joinDate),
+    ]
     
     var body: some View {
         NavigationStack {
-            UsersView(minimumJoinDate: showUpcomingOnly ? .now : .distantPast)
+            UsersView(minimumJoinDate: showUpcomingOnly ? .now : .distantPast, sortOrder: sortOrder)
             .navigationTitle("Users")
             .toolbar {
                 Button(showUpcomingOnly ? "Show Everyone" : "Show Upcoming") {
                     showUpcomingOnly.toggle()
+                }
+                
+                Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                    Picker("Sort", selection: $sortOrder) {
+                        Text("Sort by Name")
+                            .tag([
+                                SortDescriptor(\User.name),
+                                SortDescriptor(\User.joinDate),
+                            ])
+
+                        Text("Sort by Join Date")
+                            .tag([
+                                SortDescriptor(\User.joinDate),
+                                SortDescriptor(\User.name),
+                            ])
+                    }
                 }
 
                 Button("Add Samples", systemImage: "plus") {
