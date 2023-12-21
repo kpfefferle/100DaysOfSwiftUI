d8d7f16d7e71ecfa473985494fa169eccbc2e5bd
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-21 11:21:14 -0500

    Use ContentUnavailableView with action button

--- 13-Instafilter/Instafilter/ContentView.swift
+++ 13-Instafilter/Instafilter/ContentView.swift
@@ -11,7 +11,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ContentUnavailableView("No snippets", systemImage: "swift", description: Text("You don't have any saved snippets yet."))
+        ContentUnavailableView {
+            Label("No snippets", systemImage: "swift")
+        } description: {
+            Text("You don't have any saved snippets yet.")
+        } actions: {
+            Button("Create Snippet") {
+                // create a snippet
+            }
+            .buttonStyle(.borderedProminent)
+        }
     }
 }
 
