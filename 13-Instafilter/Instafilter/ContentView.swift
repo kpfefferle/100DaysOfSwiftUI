583e66867174441b35ee1457d1d1dee0a57de99c
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-21 11:19:00 -0500

    Use ContentUnavailableView

--- 13-Instafilter/Instafilter/ContentView.swift
+++ 13-Instafilter/Instafilter/ContentView.swift
@@ -10,44 +10,8 @@ import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-        }
-        .onAppear(perform: loadImage)
-    }
-    
-    func loadImage() {
-        let inputImage = UIImage(resource: .mystery)
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.twirlDistortion()
-        currentFilter.inputImage = beginImage
-
-        let amount = 1.0
-        
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
-        
-        // get a CIImage from our filter or exit if that fails
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        // attempt to get a CGImage from our CIImage
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-        
-        // convert that to a UIImage
-        let uiImage = UIImage(cgImage: cgImage)
-        
-        // and convert that to a SwiftUI Image
-        image = Image(uiImage: uiImage)
+        ContentUnavailableView("No snippets", systemImage: "swift")
     }
 }
 
