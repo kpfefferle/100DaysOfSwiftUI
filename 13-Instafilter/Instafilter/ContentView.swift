45dc3809518b2d45c2409eb694a7079491f428cd
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-21 11:07:25 -0500

    Apply twirlDistortion filter with CoreImage

--- 13-Instafilter/Instafilter/ContentView.swift
+++ 13-Instafilter/Instafilter/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Kevin Pfefferle on 12/21/23.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
@@ -20,7 +22,26 @@ struct ContentView: View {
     }
     
     func loadImage() {
-        image = Image(.mystery)
+        let inputImage = UIImage(resource: .mystery)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        currentFilter.radius = 1000
+        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+        
+        // get a CIImage from our filter or exit if that fails
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        // attempt to get a CGImage from our CIImage
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        // convert that to a UIImage
+        let uiImage = UIImage(cgImage: cgImage)
+        
+        // and convert that to a SwiftUI Image
+        image = Image(uiImage: uiImage)
     }
 }
 
