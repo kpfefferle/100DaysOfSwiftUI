24fa04b48a8230909a17fbbc6f1b1ce5fc7732e7
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-21 11:49:07 -0500

    Implement basic UI

--- 13-Instafilter/Instafilter/ContentView.swift
+++ 13-Instafilter/Instafilter/ContentView.swift
@@ -9,11 +9,34 @@ import StoreKit
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.requestReview) var requestReview
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
     
     var body: some View {
-        Button("Leave a review") {
-            requestReview()
+        NavigationStack {
+            VStack {
+                // TODO: image area
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter") {
+                        // TODO: change filter
+                    }
+                    
+                    Spacer()
+                    
+                    // TODO: share the picture
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
     }
 }
