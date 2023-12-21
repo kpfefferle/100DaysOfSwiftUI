26139e9174f65ebd59aa5811c304d88431913a21
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-21 11:59:30 -0500

    Add PhotosPicker and loadImage

--- 13-Instafilter/Instafilter/ContentView.swift
+++ 13-Instafilter/Instafilter/ContentView.swift
@@ -5,23 +5,28 @@
 //  Created by Kevin Pfefferle on 12/21/23.
 //
 
-import StoreKit
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedItem: PhotosPickerItem?
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
     
     var body: some View {
         NavigationStack {
             VStack {
-                if let processedImage {
-                    processedImage
-                        .resizable()
-                        .scaledToFit()
-                } else {
-                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
                 }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
                 
                 Spacer()
                 
@@ -46,6 +51,15 @@ struct ContentView: View {
     
     func changeFilter() {
         // TODO: change filter
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            // TODO: more code to come
+        }
     }
 }
 
