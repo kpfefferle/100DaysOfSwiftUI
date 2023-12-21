7b01ae90edb8cd8121fd467621e7196fa2a51341
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-21 10:44:50 -0500

    Init Instafilter

---
+++ 13-Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Kevin Pfefferle on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
