485d1e49a2f5e13a3b53cb894692d6f51ab880cd
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-21 10:57:05 -0500

    Use confirmationDialog to change background color

--- 13-Instafilter/Instafilter/ContentView.swift
+++ 13-Instafilter/Instafilter/ContentView.swift
@@ -8,23 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount = 0.0
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
 
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .blur(radius: blurAmount)
-            
-            Slider(value: $blurAmount, in: 0...20)
-                .onChange(of: blurAmount) { oldValue, newValue in
-                    print("New value is \(newValue)")
-                }
-            
-            Button("Random Blur") {
-                blurAmount = Double.random(in: 0...20)
-            }
+        Button("Hello, world!") {
+            showingConfirmation = true
         }
-        .padding()
+        .frame(width: 300, height: 300)
+        .background(backgroundColor)
+        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
+            Button("Red") { backgroundColor = .red }
+            Button("Green") { backgroundColor = .green }
+            Button("Blue") { backgroundColor = .blue }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Select a new color")
+        }
     }
 }
 
