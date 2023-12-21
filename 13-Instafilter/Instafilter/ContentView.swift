e2e2dc6b1ca38487222c8134bc170929281a7c06
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-21 11:00:57 -0500

    Load image as optional view

--- 13-Instafilter/Instafilter/ContentView.swift
+++ 13-Instafilter/Instafilter/ContentView.swift
@@ -8,23 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
+    @State private var image: Image?
 
     var body: some View {
-        Button("Hello, world!") {
-            showingConfirmation = true
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .frame(width: 300, height: 300)
-        .background(backgroundColor)
-        .confirmationDialog("Change background", isPresented: $showingConfirmation) {
-            Button("Red") { backgroundColor = .red }
-            Button("Green") { backgroundColor = .green }
-            Button("Blue") { backgroundColor = .blue }
-            Button("Cancel", role: .cancel) { }
-        } message: {
-            Text("Select a new color")
-        }
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        image = Image(.mystery)
     }
 }
 
