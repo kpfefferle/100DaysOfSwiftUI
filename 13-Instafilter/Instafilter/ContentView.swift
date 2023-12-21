1b904d4c913f10a14431b5b2865c9a0970226d37
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-21 12:06:14 -0500

    Process image to apply filter and adjust intensity

--- 13-Instafilter/Instafilter/ContentView.swift
+++ 13-Instafilter/Instafilter/ContentView.swift
@@ -5,12 +5,17 @@
 //  Created by Kevin Pfefferle on 12/21/23.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    let context = CIContext()
+    
     @State private var selectedItem: PhotosPickerItem?
     @State private var processedImage: Image?
+    @State private var currentFilter = CIFilter.sepiaTone()
     @State private var filterIntensity = 0.5
     
     var body: some View {
@@ -33,6 +38,7 @@ struct ContentView: View {
                 HStack {
                     Text("Intensity")
                     Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
                 }
                 .padding(.vertical)
                 
@@ -58,8 +64,20 @@ struct ContentView: View {
             guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
             guard let inputImage = UIImage(data: imageData) else { return }
             
-            // TODO: more code to come
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
         }
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
     }
 }
 
