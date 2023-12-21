38367154d9a9ec6b4208c15ab60c16c1a80ec1a8
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-21 10:52:23 -0500

    Observe value changes with onChange

--- 13-Instafilter/Instafilter/ContentView.swift
+++ 13-Instafilter/Instafilter/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
                 .blur(radius: blurAmount)
             
             Slider(value: $blurAmount, in: 0...20)
+                .onChange(of: blurAmount) { oldValue, newValue in
+                    print("New value is \(newValue)")
+                }
             
             Button("Random Blur") {
                 blurAmount = Double.random(in: 0...20)
