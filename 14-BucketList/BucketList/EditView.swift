88c9d055b740ec7361335286a856447fe1307806
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-22 16:31:13 -0500

    Load nearby places from Wikipedia on EditView

--- 14-BucketList/BucketList/EditView.swift
+++ 14-BucketList/BucketList/EditView.swift
@@ -16,6 +16,13 @@ struct EditView: View {
     @State private var name: String
     @State private var description: String
     
+    enum LoadingState {
+        case loading, loaded, failed
+    }
+    
+    @State private var loadingState = LoadingState.loading
+    @State private var pages = [Page]()
+    
     init(location: Location, onSave: @escaping (Location) -> Void) {
         self.location = location
         self.onSave = onSave
@@ -31,6 +38,23 @@ struct EditView: View {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
                 }
+                
+                Section("Nearby...") {
+                    switch loadingState {
+                    case .loaded:
+                        ForEach(pages, id: \.pageid) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") +
+                            Text("Page description here")
+                                .italic()
+                        }
+                    case .loading:
+                        Text("Loading...")
+                    case .failed:
+                        Text("Please try again later.")
+                    }
+                }
             }
             .navigationTitle("Place details")
             .toolbar {
@@ -44,6 +68,32 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
+        }
+    }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // we got some data back!
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            
+            // success - convert the array values to our pages array
+            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            loadingState = .loaded
+        } catch {
+            // if we're still here it means the reqest failed somehow
+            loadingState = .failed
         }
     }
 }
