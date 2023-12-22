2c0b599b06c5fdd9e389414a85f6dce94e3a41e3
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-22 10:39:50 -0500

    Adding Comparable conformance for sorted

---
+++ 14-BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Kevin Pfefferle on 12/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
