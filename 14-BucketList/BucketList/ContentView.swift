0235d1246a185a255de06bee1399a2ad7e4c2910
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-22 14:46:19 -0500

    Add MapMarkers to Map

--- 14-BucketList/BucketList/ContentView.swift
+++ 14-BucketList/BucketList/ContentView.swift
@@ -8,14 +8,27 @@
 import MapKit
 import SwiftUI
 
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
     @State private var mapRegion = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
         span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
     )
 
     var body: some View {
-        Map(coordinateRegion: $mapRegion)
+        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+            MapMarker(coordinate: location.coordinate)
+        }
     }
 }
 
