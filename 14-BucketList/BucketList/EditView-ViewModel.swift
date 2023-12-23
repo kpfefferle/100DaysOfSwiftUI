3bd174268e4a9f4a0158502ab7fb25d2b1bc2fff
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-23 00:14:01 -0500

    Extract EditView ViewModel

---
+++ 14-BucketList/BucketList/EditView-ViewModel.swift
@@ -0,0 +1,66 @@
+//
+//  File.swift
+//  BucketList
+//
+//  Created by Kevin Pfefferle on 12/23/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension EditView {
+    @MainActor @Observable class ViewModel: ObservableObject {
+        var location: Location
+        var onSave: (Location) -> Void
+            
+        var name: String
+        var description: String
+
+        enum LoadingState {
+            case loading, loaded, failed
+        }
+        
+        var loadingState = LoadingState.loading
+        private(set) var pages = [Page]()
+        
+        init(location: Location, onSave: @escaping (Location) -> Void) {
+            self.location = location
+            self.onSave = onSave
+
+            self.name = location.name
+            self.description = location.description
+        }
+        
+        func saveNewLocation() {
+            var newLocation = location
+            newLocation.id = UUID()
+            newLocation.name = name
+            newLocation.description = description
+            
+            onSave(newLocation)
+        }
+
+        func fetchNearbyPlaces() async {
+            let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+            
+            guard let url = URL(string: urlString) else {
+                print("Bad URL: \(urlString)")
+                return
+            }
+            
+            do {
+                let (data, _) = try await URLSession.shared.data(from: url)
+                
+                // we got some data back!
+                let items = try JSONDecoder().decode(Result.self, from: data)
+                
+                // success - convert the array values to our pages array
+                pages = items.query.pages.values.sorted()
+                loadingState = .loaded
+            } catch {
+                // if we're still here it means the reqest failed somehow
+                loadingState = .failed
+            }
+        }
+    }
+}
