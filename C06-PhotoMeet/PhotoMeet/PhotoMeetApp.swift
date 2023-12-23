30c21572113775e3da2413f5e5619634366884f7
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-23 16:52:12 -0500

    Init PhotoMeet

---
+++ C06-PhotoMeet/PhotoMeet/PhotoMeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoMeetApp.swift
+//  PhotoMeet
+//
+//  Created by Kevin Pfefferle on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoMeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
