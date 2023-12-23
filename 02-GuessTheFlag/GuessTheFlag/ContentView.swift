f8cc9a0baead469e76ff5671bc2c7ea96f26de84
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-23 14:34:40 -0500

    Use accessibilityLabel to describe flags

--- 02-GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 02-GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -25,6 +25,19 @@ struct FlagImage: View {
 
 struct ContentView: View {
     private let gameLength = 8
+    private let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
     
     @State private var answerCount = 0
     @State private var score = 0
@@ -84,6 +97,7 @@ struct ContentView: View {
                                 rotateAmount: (number == selectedFlag ? 360.0 : 0.0),
                                 scaleAmount: ((selectedFlag == -1 || number == selectedFlag) ? 1.0 : 0.8)
                             )
+                            .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                         }
                     }
                 }
