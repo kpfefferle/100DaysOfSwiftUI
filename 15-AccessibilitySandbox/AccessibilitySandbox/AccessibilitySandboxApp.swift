971d13579a32c5b1609f7c841757f993d0484f70
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-23 14:06:57 -0500

    Init AccessibilitySandbox

---
+++ 15-AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Kevin Pfefferle on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
