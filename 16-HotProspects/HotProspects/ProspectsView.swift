73ea001054f0cf015559139535529ed4e92afce5
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-24 14:35:25 -0500

    Add filter enum for ProspectsView

--- 16-HotProspects/HotProspects/ProspectsView.swift
+++ 16-HotProspects/HotProspects/ProspectsView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct ProspectsView: View {
+    enum FilterType {
+        case none, contacted, uncontacted
+    }
+    
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle(title)
+        }
     }
 }
 
 #Preview {
-    ProspectsView()
+    ProspectsView(filter: .none)
 }
