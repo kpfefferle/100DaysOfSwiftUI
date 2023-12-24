495339f74b4d65f4435e63958c4587df2c64453d
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-24 12:10:59 -0500

    Simple contextMenu

--- 16-HotProspects/HotProspects/ContentView.swift
+++ 16-HotProspects/HotProspects/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var backgroundColor = Color.red
+    
     var body: some View {
-        Image(.example)
-            .interpolation(.none)
-            .resizable()
-            .scaledToFit()
-            .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-            .background(.black)
-            .ignoresSafeArea()
+        VStack {
+            Text("Hello, World!")
+                .padding()
+                .background(backgroundColor)
+            
+            Text("Change Color")
+                .padding()
+                .contextMenu {
+                    Button("Red") {
+                        backgroundColor = .red
+                    }
+                    Button("Green") {
+                        backgroundColor = .green
+                    }
+                    Button("Blue") {
+                        backgroundColor = .blue
+                    }
+                }
+        }
     }
 }
 
