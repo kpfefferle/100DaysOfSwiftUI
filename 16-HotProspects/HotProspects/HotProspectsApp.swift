4f0d2d322526ea8aa6ac0589d6faeb4f65370a14
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-23 23:25:49 -0500

    Init HotProspects

---
+++ 16-HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Kevin Pfefferle on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
