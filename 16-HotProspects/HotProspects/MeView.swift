a1e91e62785ed4f84e93e9b9179287e0e6f55ca5
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-24 15:23:40 -0500

    Lay out MeView with generated QR Code

--- 16-HotProspects/HotProspects/MeView.swift
+++ 16-HotProspects/HotProspects/MeView.swift
@@ -5,11 +5,47 @@
 //  Created by Kevin Pfefferle on 12/24/23.
 //
 
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct MeView: View {
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    @State private var name = "Anonymous"
+    @State private var emailAddress = "you@yoursite.com"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                
+                TextField("Email address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .navigationTitle("Your code")
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+        
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
