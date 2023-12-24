e28bdf0f7d2402e51a951567706ceb5380e6a4d9
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-24 14:31:22 -0500

    Create TabView

---
+++ 16-HotProspects/HotProspects/MeView.swift
@@ -0,0 +1,18 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Kevin Pfefferle on 12/24/23.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeView()
+}
---
+++ 16-HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Kevin Pfefferle on 12/24/23.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProspectsView()
+}
