b03b5268fbbfbe5c876360c40e0a25817656d8cf
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-23 23:31:44 -0500

    Programmatic TabView

--- 16-HotProspects/HotProspects/ContentView.swift
+++ 16-HotProspects/HotProspects/ContentView.swift
@@ -8,16 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedTab = "One"
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             Text("Tab 1")
+                .onTapGesture {
+                    selectedTab = "Two"
+                }
                 .tabItem {
                     Label("One", systemImage: "star")
                 }
+
             Text("Tab 2")
                 .tabItem {
                     Label("Two", systemImage: "circle")
                 }
+                .tag("Two")
         }
     }
 }
