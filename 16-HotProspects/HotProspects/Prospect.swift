7b1b917aeea4b3f88fcf2fea3a2a9842e7373d3c
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-24 18:13:29 -0500

    Toggle isContacted with swipeAction

--- 16-HotProspects/HotProspects/Prospect.swift
+++ 16-HotProspects/HotProspects/Prospect.swift
@@ -11,7 +11,7 @@ class Prospect: Identifiable, Codable {
     var id = UUID()
     var name = "Anonymous"
     var emailAddress = ""
-    var isContacted = false
+    fileprivate(set) var isContacted = false
 }
 
 @MainActor
@@ -21,5 +21,10 @@ class Prospects: ObservableObject {
     
     init() {
         self.people = []
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
     }
 }
