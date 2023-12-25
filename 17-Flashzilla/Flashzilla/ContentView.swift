884419290f07980f94d91bc42def4142c76e825c
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-25 00:15:55 -0500

    Use contentShape to expand hit area

--- 17-Flashzilla/Flashzilla/ContentView.swift
+++ 17-Flashzilla/Flashzilla/ContentView.swift
@@ -20,10 +20,10 @@ struct ContentView: View {
             Circle()
                 .fill(.red)
                 .frame(width: 300, height: 300)
+                .contentShape(Rectangle())
                 .onTapGesture {
                     print("Circle tapped!")
                 }
-                .allowsHitTesting(false)
         }
     }
 }
