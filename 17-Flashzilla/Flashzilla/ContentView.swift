aafc48b11ee6ec44527cab5b99f56fd04f3196b8
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-26 12:28:28 -0500

    Pause timer when app is not active

--- 17-Flashzilla/Flashzilla/ContentView.swift
+++ 17-Flashzilla/Flashzilla/ContentView.swift
@@ -16,6 +16,9 @@ extension View {
 
 struct ContentView: View {
     @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.scenePhase) var scenePhase
+    
+    @State private var isActive = true
     
     @State private var timeRemaining = 100
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
@@ -71,8 +74,17 @@ struct ContentView: View {
             }
         }
         .onReceive(timer) { time in
+            guard isActive else { return }
+
             if timeRemaining > 0 {
                 timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) { newPhase in
+            if newPhase == .active {
+                isActive = true
+            } else {
+                isActive = false
             }
         }
     }
