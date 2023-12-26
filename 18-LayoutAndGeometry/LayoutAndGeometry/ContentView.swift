eff30fb1afc0ecccf9c04f1091606b2b8638b38b
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-26 15:58:30 -0500

    Using GeometryReader for percentage based sizing

--- 18-LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 18-LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .offset(x: 100, y: 100)
-            .background(.red)
+        GeometryReader { geo in
+            Text("Hello, world!")
+                .frame(width: geo.size.width * 0.9)
+                .background(.red)
+        }
     }
 }
 
