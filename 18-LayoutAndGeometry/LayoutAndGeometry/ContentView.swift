b2872db6d9fe72955974c5d9c0047aba276fe6c7
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-26 15:54:31 -0500

    Absolute positioning

--- 18-LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 18-LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,35 +7,11 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue(in d: ViewDimensions) -> CGFloat {
-            d[.top]
-        }
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@twostraws")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image(systemName: "person.fill")
-                    .resizable()
-                    .frame(width: 64, height: 64)
-                    .background(.gray.opacity(0.5))
-            }
-            
-            VStack {
-                Text("Full name:")
-                Text("PAUL HUDSON")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello, world!")
+            .position(x: 100, y: 100)
+            .background(.red)
     }
 }
 
