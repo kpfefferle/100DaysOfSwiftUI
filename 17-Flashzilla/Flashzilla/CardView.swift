478bc340000351b757e219651357ee66782b4fad
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-25 23:47:18 -0500

    Create Card model and CardView

---
+++ 17-Flashzilla/Flashzilla/CardView.swift
@@ -0,0 +1,44 @@
+//
+//  CardView.swift
+//  Flashzilla
+//
+//  Created by Kevin Pfefferle on 12/25/23.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+    
+    @State private var isShowingAnswer = false
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(.white)
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            
+            VStack {
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundStyle(.black)
+                
+                if isShowingAnswer {
+                    Text(card.answer)
+                        .font(.title)
+                        .foregroundStyle(.gray)
+                }
+            }
+            .padding(20)
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
+    }
+}
+
+#Preview {
+    CardView(card: Card.example)
+}
