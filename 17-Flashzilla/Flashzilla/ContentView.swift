9b59bc0d3a49d17dd8e090afa179db321cd35c79
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-26 12:50:06 -0500

    Mark background image as decorative

--- 17-Flashzilla/Flashzilla/ContentView.swift
+++ 17-Flashzilla/Flashzilla/ContentView.swift
@@ -27,7 +27,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Image(.background)
+            Image(decorative: "background")
                 .resizable()
                 .ignoresSafeArea()
             
