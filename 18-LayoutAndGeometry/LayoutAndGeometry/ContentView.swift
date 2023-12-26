2b7c90a0f7c4bc38f74224ac495d58b0b02d8907
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-26 15:22:21 -0500

    Customize alignmentGuide

--- 18-LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ 18-LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,15 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .lastTextBaseline) {
-            Text("Live")
-                .font(.caption)
-            Text("long")
-            Text("and")
-                .font(.title)
-            Text("prosper")
-                .font(.largeTitle)
+        VStack(alignment: .leading) {
+            Text("Hello, world!")
+                .alignmentGuide(.leading) { d in d[.trailing] }
+            Text("This is a longer line of text")
         }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
     }
 }
 
