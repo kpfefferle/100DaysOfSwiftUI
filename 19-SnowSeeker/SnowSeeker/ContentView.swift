25b8cb9f2e8d5baaea650505fc041cc1c5f0cd3d
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-27 00:16:38 -0500

    Serchable NavigationView

--- 19-SnowSeeker/SnowSeeker/ContentView.swift
+++ 19-SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,27 +7,14 @@
 
 import SwiftUI
 
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Luna and Arya")
-        }
-        .font(.title)
-    }
-}
-
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var sizeClass
+    @State private var searchText = ""
     
     var body: some View {
-        Group {
-            if sizeClass == .compact {
-                VStack(content: UserView.init)
-            } else {
-                HStack(content: UserView.init)
-            }
+        NavigationView {
+            Text("Searching for \(searchText)")
+                .searchable(text: $searchText, prompt: "Look for something")
+                .navigationTitle("Searching")
         }
     }
 }
