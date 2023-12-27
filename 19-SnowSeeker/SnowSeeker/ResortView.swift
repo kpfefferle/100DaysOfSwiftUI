0563e716f909b531fef5f4a84f54c4c8b5f9831a
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-27 10:20:14 -0500

    Add ResortView for resort details

---
+++ 19-SnowSeeker/SnowSeeker/ResortView.swift
@@ -0,0 +1,40 @@
+//
+//  ResortView.swift
+//  SnowSeeker
+//
+//  Created by Kevin Pfefferle on 12/27/23.
+//
+
+import SwiftUI
+
+struct ResortView: View {
+    let resort: Resort
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+                Image(decorative: resort.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Group {
+                    Text(resort.description)
+                        .padding(.vertical)
+                    
+                    Text("Facilities")
+                        .font(.headline)
+                    
+                    Text(resort.facilities.joined(separator: ", "))
+                        .padding(.vertical)
+                }
+                .padding(.horizontal)
+            }
+        }
+        .navigationTitle("\(resort.name), \(resort.country)")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    ResortView(resort: Resort.example)
+}
