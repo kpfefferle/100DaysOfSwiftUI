c13958eafc0409212790d75b9e15d7203d343c13
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-27 00:11:10 -0500

    Group as transparent layout

--- 19-SnowSeeker/SnowSeeker/ContentView.swift
+++ 19-SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,21 +7,35 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna and Arya")
+        }
+        .font(.title)
+    }
 }
 
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
-    @State private var isShowingUser = false
-
+    @State private var layoutVertically = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .onTapGesture {
-                selectedUser = User()
-                isShowingUser = true
+        Group {
+            if layoutVertically {
+                VStack {
+                    UserView()
+                }
+            } else {
+                HStack {
+                    UserView()
+                }
             }
-            .alert("Welcome", isPresented: $isShowingUser) { }
+        }
+        .onTapGesture {
+            layoutVertically.toggle()
+        }
     }
 }
 
