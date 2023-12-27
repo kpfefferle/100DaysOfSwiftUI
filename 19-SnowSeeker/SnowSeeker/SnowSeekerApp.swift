1ef9463912d4f16815df6e2df05505f57115cf0a
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-26 23:57:49 -0500

    Init SnowSeeker

---
+++ 19-SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Kevin Pfefferle on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
