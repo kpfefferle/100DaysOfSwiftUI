9c235d82d05da58606b3568f003e63ed2c627c59
Kevin Pfefferle <kevin@pfefferle.me>
2023-12-27 00:04:45 -0500

    Sheet triggered by optional value

--- 19-SnowSeeker/SnowSeeker/ContentView.swift
+++ 19-SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,15 +7,21 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct ContentView: View {
+    @State private var selectedUser: User? = nil
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello, world!")
+            .onTapGesture {
+                selectedUser = User()
+            }
+            .sheet(item: $selectedUser) { user in
+                Text(user.id)
+            }
     }
 }
 
